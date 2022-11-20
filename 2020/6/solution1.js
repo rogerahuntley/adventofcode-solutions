46ca5084b6411ae0a5a2a6e97d022f439b9176e6
Roger Huntley <rogerh@hesonline.com>
2022-11-20 16:41:17 -0500

    2020/6 solutions

---
+++ 2020/6/solution1.js
@@ -0,0 +1,8 @@
+const groupAnswers = input.trim().split("\n\n").map(g => new Set(g.replaceAll('\n','').split('').sort()))
+// we merge group answers and then count the total
+
+let total = 0;
+groupAnswers.forEach(g => {
+  total += g.size
+})
+return total;
---
+++ 2020/6/solution2.js
@@ -0,0 +1,18 @@
+const groups = input.trim().split("\n\n").map(g => g.split('\n').map(gm => new Set(gm.split('').sort())))
+// we merge answered shared among all group members and then count the total
+
+const groupAnswers = groups.map(g => {
+  // we're looking for answers from all members, so we can just use the first as a reference index
+  const [first, ...rest] = g
+  rest.forEach(ga => {
+    first.forEach(a =>{
+      if(!ga.has(a)){
+        // we'll loop through the answers from the "others" and remove the answers from the first member if it isn't found
+        first.delete(a)
+      }
+    })
+  })
+  return first
+})
+
+return groupAnswers.reduce((total, ga) => total + ga.size, 0)
