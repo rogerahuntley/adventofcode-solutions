4be7833218760431698cd0a09e2d779399512489
Roger Huntley <rogerh@hesonline.com>
2023-03-13 07:23:55 -0400

    add 2015/4

---
+++ 2015/4/solution1.js
@@ -0,0 +1,20 @@
+const string = input.trim()
+// start with string, add numbers and get MD5 hash until hash starts with 5 zeroes
+
+let i = 0
+let found = false
+
+const startsWith = '00000'
+
+// loop through, increment i, and brute force hash
+while (!found) {
+  const _string = string + i
+  const hash = CryptoJS.MD5(_string).toString()
+  if (hash.startsWith(startsWith)) {
+    found = true
+  } else {
+    i++
+  }
+}
+
+return i
